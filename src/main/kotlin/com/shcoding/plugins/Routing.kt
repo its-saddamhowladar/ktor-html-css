3cd1df9cdaa7b20e638490c873ad729a78f1ffbf
saddam_howladar_gt <saddamhowladar757@gmail.com>
2024-11-07 11:00:26 +0600

    initial commit

--- src/main/kotlin/com/shcoding/plugins/Routing.kt
+++ src/main/kotlin/com/shcoding/plugins/Routing.kt
@@ -1,16 +1,13 @@
 package com.shcoding.plugins
 
 import io.ktor.http.*
-import io.ktor.resources.*
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.resources.*
-import io.ktor.server.resources.Resources
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.webjars.*
-import kotlinx.serialization.Serializable
 
 fun Application.configureRouting() {
     install(Webjars) {
